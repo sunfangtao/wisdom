7e7037e8be3d81f0791062b14bd5979a419e1f10
sunfangtao <sunfangtao@aioute.com>
2017-07-26 17:57:41 +0800

    第一次提交（覆盖）

--- app/src/main/java/com/sft/wisdom/activity/IndexActivity.java
+++ app/src/main/java/com/sft/wisdom/activity/IndexActivity.java
@@ -1,5 +1,9 @@
 package com.sft.wisdom.activity;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+
 import com.sft.wisdom.R;
 import com.sft.wisdom.base.BActivity;
 
@@ -7,4 +11,8 @@ import org.androidannotations.annotations.EActivity;
 
 @EActivity(R.layout.activity_index)
 public class IndexActivity extends BActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
 }
